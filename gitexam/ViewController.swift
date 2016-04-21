555002b6fe4179c003f3d1e39cd7abdb4ac5d5c5
Sarah Little <sarah.little01@gmail.com>
2016-04-20 22:33:44 -0400

    Initial Commit

---
+++ gitexam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitexam
+//
+//  Created by Sarah Little on 4/20/16.
+//  Copyright © 2016 Chip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
